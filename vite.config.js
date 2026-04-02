567613574e09a2226880a5c1cb16514bd03d94fd
PhaseOfficial <97903370+PhaseOfficial@users.noreply.github.com>
2026-04-02 14:34:37 +0200

    feat: add SEO enhancements with meta tags, manifest, robots.txt, and sitemap; update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kotaitems/", // Set the base path for GitHub Pages deployment
+  base: "/", // Set the base path for GitHub Pages deployment
   
 });
